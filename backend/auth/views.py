9ffd61899da6dea23e1b107ed6a6a09691794e7a
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-12-09 11:40:16 +0800

    Initialize backend

---
+++ backend/auth/views.py
@@ -0,0 +1,10 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from django.conf import settings
+
+class GoogleLogin(SocialLoginView):
+    authentication_classes = [] # disable authentication
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "localhost:3000"
+    client_class = OAuth2Client
